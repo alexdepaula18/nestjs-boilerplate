946fbc1dd9980710ecd9459fdc5fd776cf4ea8d7
Alex de Paula <alex.paula@decodev.com.br>
2022-02-04 22:11:21 -0300

    Adding health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, HealthCheckModule],
 })
 export class AppModule {}
